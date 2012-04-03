9284e517f9aaad7bc9278605b10688199a535aea
Blake Hilscher <blake@hilscher.ca>
2012-04-03 13:06:20 -0400

    ran html5:install and scss_boilerplate:install

---
+++ config/compass.rb
@@ -0,0 +1,18 @@
+# This configuration file works with both the Compass command line tool and within Rails.
+require 'compass/h5bp'
+
+# Require any additional compass plugins here.
+
+project_type = :rails
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+
+# You can select your preferred output style here (can be overridden via the command line):
+output_style = :compact# or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
