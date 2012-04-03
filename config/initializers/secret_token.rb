b7e2905e7387e370d4f96eb8ce0ae6b82f84cde0
Blake Hilscher <blake@hilscher.ca>
2012-04-03 11:45:14 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlakeHilscherCa::Application.config.secret_token = 'f03d05a172cf396ad56144d03934787f11fa419931958f0abb23dca6bf95ba52b47832f9cca5f8b1dd6b91cf74728f8a097ddc48b259813239acf76b3c6e865f'
