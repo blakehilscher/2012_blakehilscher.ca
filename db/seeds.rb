0238e4901736030b5a5012f1bc8d5cc2fd698b4f
Blake H <blake@indusblue.com>
2012-04-07 15:56:59 -0400

    updated readme

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 user = User.where(:email => 'blake@hilscher.ca').first
-user = User.create!({:email => 'blake@hilscher.ca', :password => 'blake99', :password_confirmation => 'blake99'}) if user.blank?
+user = User.create!({:email => 'blake@hilscher.ca', :password => 'default', :password_confirmation => 'default'}) if user.blank?
 
 20.times do
   user.posts.create!({:content => Faker::Lorem.paragraphs.join("\n"), :title => Faker::Lorem.words(5).join(' ').humanize })
